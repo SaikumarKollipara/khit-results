e8e4af61d2024c4abccdcdd7ed61349fb6ed59c7
Saikumarkollipara <krksaikumar@gmail.com>
2023-05-18 12:13:53 +0530

    fixing cors issue

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,10 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+}));
 
 
 // Route handlers
